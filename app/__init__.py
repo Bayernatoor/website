e141595e7c648349e34d5eb6b23d5d2195ba0d0c
bayer <bayernator@protonmail.com>
2022-05-01 00:01:41 +0200

    refactor styling using flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 mail = Mail(app)
@@ -16,6 +16,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+bootstrap = Bootstrap(app)
 
 from app import routes, models, errors
 
