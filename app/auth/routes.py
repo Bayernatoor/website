2c1679224b04ab64cd46e1bc4c2b1f736e7bda74
bayer <bayernator@protonmail.com>
2022-06-06 23:21:11 +0200

    add some logging

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for, flash, request
+import logging
+from flask import render_template, redirect, url_for, flash, request, current_app
 from werkzeug.urls import url_parse
 from flask_login import login_user, logout_user, current_user
 from flask_babel import _
@@ -8,6 +9,15 @@ from app.auth.forms import LoginForm, RegistrationForm, \
     ResetPasswordRequestForm, ResetPasswordForm
 from app.models import User
 from app.auth.email import send_password_reset_email
+from logging.handlers import WatchedFileHandler
+
+@bp.before_app_first_request
+def setup_logging():
+    """ SETUP LOGGING """
+    handler = WatchedFileHandler("logs/website.log")
+    current_app.logger.addHandler(handler)
+    current_app.logger.setLevel(logging.INFO)
+
 
 
 @bp.route('/login', methods=['GET', 'POST'])
