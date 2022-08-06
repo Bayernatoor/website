05ccc8b9e3fcf9b5bdaaff51626ba55bf87de6e3
bayer <bayernator@protonmail.com>
2022-08-06 17:39:50 +0200

    finish private message functionality

--- app/models.py
+++ app/models.py
@@ -1,10 +1,12 @@
+import json
+import jwt
 from datetime import datetime
+from time import time
 from hashlib import md5
 from time import time
 from flask import current_app
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
 from app import db, login
 from app.serch import add_to_index, remove_from_index, query_index
 
@@ -79,6 +81,8 @@ class User(UserMixin, db.Model):
                                         foreign_keys='Message.recipient_id',
                                         backref='recipient', lazy='dynamic')
     last_message_read_time = db.Column(db.DateTime)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
+
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -132,6 +136,12 @@ class User(UserMixin, db.Model):
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
 
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
+
 
 @login.user_loader
 def load_user(id):
@@ -158,3 +168,13 @@ class Message(db.Model):
 
     def __repr__(self):
         return f'Message {self.body}'
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
